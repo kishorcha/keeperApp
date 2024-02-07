ad0aa8bb1fa0118c32e8579eb74b743ed65dacca
Your Name <118709199+kishorcha@users.noreply.github.com>
2024-02-07 19:49:28 +0530

    first commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import Header from "./Header";
-import Footer from "./footer";
-import Note from "./Note";
-import CreateArea from "./CreateArea";
+import Header from "./components/Header";
+import Footer from "./components/footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
 import './style.css'
 
 
